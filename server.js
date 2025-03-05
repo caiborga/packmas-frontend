993c7671ab4c9b5f3b703b9287326be3b8f10da5
Stephan Kleinhans <4.da.crap@gmail.com>
2025-03-05 07:50:09 +0100

    set CORS-Header

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://pack-mas-5525b94af9e0.herokuapp.com");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    next();
+});
+
+app.use(express.static(__dirname + "/dist"));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 
 app.listen(process.env.PORT || 4200);
