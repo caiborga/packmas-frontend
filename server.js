fc11303dc38354d6a9bbaa35879a8b743f8c8a68
Stephan Kleinhans <4.da.crap@gmail.com>
2025-03-05 08:14:59 +0100

    changed dir path

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const path = require("path");
 
 const app = express();
 
+var distDir = __dirname + "/dist/";
+
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://pack-mas-5525b94af9e0.herokuapp.com");
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
@@ -16,9 +17,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/browser"));
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/dist/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/browser/index.html"));
 });
 
 app.listen(process.env.PORT || 4200);
